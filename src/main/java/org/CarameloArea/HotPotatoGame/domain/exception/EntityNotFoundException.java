12740d1e2c2a8a3a33c77c7bc140efcae91dcafb
CaweAlves <contato@cawe.dev>
2025-08-19 09:03:31 -0300

    refactor(exception): create abstract base class for alert exceptions

--- src/main/java/org/CarameloArea/HotPotatoGame/domain/exception/EntityNotFoundException.java
+++ src/main/java/org/CarameloArea/HotPotatoGame/domain/exception/EntityNotFoundException.java
@@ -1,8 +1,8 @@
 package org.CarameloArea.HotPotatoGame.domain.exception;
 
-import org.CarameloArea.HotPotatoGame.infrastructure.adapter.rest.exception.BadRequestAlertException;
+import org.CarameloArea.HotPotatoGame.infrastructure.adapter.rest.exception.NotFoundAlertException;
 
-public class EntityNotFoundException extends BadRequestAlertException {
+public class EntityNotFoundException extends NotFoundAlertException {
 
     public EntityNotFoundException(String entity) {
         super(entity + " not found.", entity, "entity-not-found");
--- src/main/java/org/CarameloArea/HotPotatoGame/infrastructure/adapter/rest/exception/ConflictAlertException.java
+++ src/main/java/org/CarameloArea/HotPotatoGame/infrastructure/adapter/rest/exception/ConflictAlertException.java
@@ -1,42 +1,10 @@
 package org.CarameloArea.HotPotatoGame.infrastructure.adapter.rest.exception;
 
-import org.zalando.problem.AbstractThrowableProblem;
 import org.zalando.problem.Status;
 
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-
-public class ConflictAlertException extends AbstractThrowableProblem {
-
-    private static final long serialVersionUID = 1L;
-
-    private final String entityName;
-
-    private final String errorKey;
+public class ConflictAlertException extends AlertException {
 
     public ConflictAlertException(String defaultMessage, String entityName, String errorKey) {
-        this(ErrorConstants.DEFAULT_TYPE, defaultMessage, entityName, errorKey);
-    }
-
-    public ConflictAlertException(URI type, String defaultMessage, String entityName, String errorKey) {
-        super(type, defaultMessage, Status.CONFLICT, null, null, null, getAlertParameters(entityName, errorKey));
-        this.entityName = entityName;
-        this.errorKey = errorKey;
-    }
-
-    public String getEntityName() {
-        return entityName;
-    }
-
-    public String getErrorKey() {
-        return errorKey;
-    }
-
-    private static Map<String, Object> getAlertParameters(String entityName, String errorKey) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("message", "error." + errorKey);
-        parameters.put("params", entityName);
-        return parameters;
+        super(Status.CONFLICT, defaultMessage, entityName, errorKey);
     }
 }
